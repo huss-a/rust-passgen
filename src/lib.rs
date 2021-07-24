306b5fa9ab1eea9c5ac206a84403c0a8ea7233c0
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-07-24 16:45:44 +0300

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub struct PassConfig {
     pub length: u32,
     pub has_nums: bool,
     pub has_symbols: bool,
-    pub filename: String,
+    pub filename: Option<String>,
 }
 
 pub fn create_pass(config: PassConfig) -> String {
@@ -38,44 +38,43 @@ pub fn create_pass(config: PassConfig) -> String {
     gen_pass(length, chars, filename)
 }
 
-fn gen_pass(length: u32, chars: String, filename: String) -> String {
+fn gen_pass(length: u32, chars: String, filename: Option<String>) -> String {
     let mut password = String::new();
-
+    let char_len = chars.len();
     for _ in 0..length {
-        let random = rand::thread_rng().gen_range(0..chars.len());
-        let char_at_rand_index = &(chars.chars().collect::<Vec<char>>()[random]).to_string();
-        password.push_str(char_at_rand_index);
+        let random = rand::thread_rng().gen_range(0..char_len);
+        let char_at_rand_index = chars.chars().nth(random).unwrap();
+        password.push(char_at_rand_index);
     }
 
-    if filename != "" {
-        save_pass(filename, password.clone());
+    if let Some(filename) = filename {
+        save_pass(Some(filename), password.clone());
     }
 
     password
 }
 
-fn save_pass(filename: String, password: String) {
+fn save_pass(filename: Option<String>, password: String) {
     let mut color_config = ColorConfig::new(Color::Magenta);
     color_config.set_is_bold(true);
     let mut stdout = std_color(&color_config);
 
-    if filename != "".to_string() {
-        writeln!(&mut stdout, "Saved password to `{}`!", filename).unwrap();
-    };
-
-    match fs::write(filename, password) {
-        Ok(()) => (),
-        Err(e) => {
-            color_config.set_fg(Color::Red);
-            writeln!(&mut stdout, "Error while writing to file: {}", e).unwrap();
-
-            std::process::exit(1);
-        }
+    if let Some(name) = filename {
+        writeln!(&mut stdout, "Saved password to `{}`!", name).unwrap();
+        match fs::write(name, password) {
+            Ok(()) => (),
+            Err(e) => {
+                color_config.set_fg(Color::Red);
+                writeln!(&mut stdout, "Error while writing to file: {}", e).unwrap();
+                std::process::exit(1);
+            }
+        };
     };
 }
 
 pub fn run(config: Vec<String>) {
-    let (mut has_nums, mut has_symbols, mut length, mut filename) = (true, true, 8, String::new());
+    let (mut has_nums, mut has_symbols, mut length, mut filename, help_message) =
+        (true, true, 8, None, include_str!("help_message.txt"));
 
     for (index, arg) in config.iter().enumerate() {
         match &arg[..] {
@@ -86,13 +85,13 @@ pub fn run(config: Vec<String>) {
                 has_symbols = false;
             }
             "--out" | "-o" => {
-                filename = config[index + 1].to_string();
+                filename = Some(config[index + 1].to_string());
             }
             "--len" | "-l" => {
                 length = config[index + 1].parse().unwrap();
             }
             "--help" | "-h" => {
-                println!("{}", fs::read_to_string("src/help_message.txt").unwrap());
+                println!("{}", help_message);
                 std::process::exit(1);
             }
             _ => continue,
